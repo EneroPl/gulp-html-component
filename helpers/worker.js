dc96bd5617de95c9f9964cd8e48ddde47978cf30
e.lankovich <e.lankovich@ddos-guard.net>
2022-12-06 14:34:16 +0300

    Refactoring index.js

---
+++ helpers/worker.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const gutil = require("gulp-util");
+
+module.exports = ({ file, encoding, path }) => ({
+  components() {
+    try {
+      const BASE_DIR = path + "/components";
+
+      return fs
+        .readdirSync(BASE_DIR, { withFileTypes: true })
+        .map(({ name: filename }) => {
+          const [name, format] = filename.split(".");
+
+          return {
+            name,
+            format,
+            filename,
+            component: fs.readFileSync(`${BASE_DIR}/${filename}`, encoding),
+          };
+        })
+        .filter(({ format }) => format === "html");
+    } catch (err) {
+      console.log(err.message);
+      new gutil.PluginError(
+        "gulp-html-component",
+        `Path "${path}/components" not found.`
+      );
+    }
+  },
+  parseProps(tag) {
+    try {
+      console.log(tag);
+      return (
+        tag.match(/p-[a-zA-Z]+=".*"/gm)?.reduce((acc, prop) => {
+          let [key, value] = prop.split("=");
+
+          key = key.split("-")[1];
+          value = JSON.parse(value);
+
+          acc[key] = value;
+          return acc;
+        }, {}) || {}
+      );
+    } catch (err) {
+      return {};
+    }
+  },
+  useProps(component, props) {
+    component = component.toString();
+
+    Object.entries(props).forEach(([key, value]) => {
+      component = component.replace(new RegExp(`{{\\s*?${key}\\s*?}}`, "gm"), value);
+    });
+
+    return new Buffer.from(component, encoding);
+  },
+});
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require("fs");
 const gutil = require("gulp-util");
 const through = require("through2");
+const Worker = require("./helpers/worker");
 
 const initialOptions = {
   path: "./src",
@@ -9,7 +10,7 @@ const initialOptions = {
 };
 
 module.exports = (options = initialOptions) => {
-  const BASE_DIR = options.path + "/components";
+  const worker = Worker(options);
 
   return through.obj(function (file, enc, cb) {
     if (file.isNull()) {
@@ -19,59 +20,29 @@ module.exports = (options = initialOptions) => {
 
     if (file.isStream()) {
       cb(
-        new gutil.PluginError("gulp-html-components", "Streaming not supported")
+        new gutil.PluginError("gulp-html-component", "Streaming not supported")
       );
       return;
     }
 
     try {
-      const data = file.contents.toString();
+      worker.components().forEach(({ name, component }) => {
+        const handler = file.contents
+          .toString()
+          .replace(
+            new RegExp(`<${name}(.*)?(\\/)?>(.*<\\/${name}>)?`, "gm"),
+            (tag) => {
+              if (file.contents.includes(name)) {
+                return worker.useProps(component, worker.parseProps(tag));
+              }
+            }
+          );
 
-      fs.readdir(BASE_DIR, { withFileTypes: true }, (_, components) => {
-        components.forEach((component) => {
-          const [name] = component.name.split(".");
-
-          if (data.includes(name)) {
-            file.contents = new Buffer(
-              file.contents
-                .toString()
-                .replace(
-                  new RegExp(`<${name}(.*)?(\\/)?>(.*<\\/${name}>)?`, "gm"),
-                  (matched) => {
-                    const props =
-                      matched
-                        .match(/p-[a-zA-Z]+=".*"/gm)
-                        ?.reduce((acc, prop) => {
-                          let [key, value] = prop.split("=");
-
-                          key = key.split("-")[1];
-                          value = JSON.parse(value);
-
-                          acc[key] = value;
-                          return acc;
-                        }, {}) || {};
-
-                    let content = fs
-                      .readFileSync(BASE_DIR + "/" + component.name)
-                      .toString();
-
-                    Object.entries(props).forEach(([key, value]) => {
-                      content = content.replace(
-                        new RegExp(`{{\\s*?${key}\\s*?}}`, "gm"),
-                        value
-                      );
-                    });
-
-                    return new Buffer.from(content, options.encoding);
-                  }
-                )
-            );
-          }
-        });
-
-        this.push(file);
-        cb();
+        file.contents = new Buffer.from(handler, options.encoding);
       });
+
+      this.push(file);
+      cb();
     } catch (err) {
       this.emit("error", err);
     }
